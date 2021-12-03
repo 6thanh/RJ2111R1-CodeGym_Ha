8634e61e69c8814f6306b8b32593425874e75374
6thanh <sauthanh106@gmail.com>
2021-12-03 13:36:12 +0700

    add module

---
+++ WebAppBuilding/ES6/PRACTICE/useModule/main.js
@@ -0,0 +1,13 @@
+let greet = "Xin chào CODEGYM ONLINE!";
+const LESSON = "Sử dụng Module trong ES6";
+
+function greeting(name, content) {
+    return name + " " + content;
+}
+
+// Xuất các biến và hàm
+export {
+    greet,
+    LESSON,
+    greeting
+};
